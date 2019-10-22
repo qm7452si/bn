04a89af28b8b23ac78d729812b604cefe84f820b
qm7452si <56890156+qm7452si@users.noreply.github.com>
2019-10-22 13:13:39 -0500

    Add files via upload

---
+++ WattersPE5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WattersPE5
+{
+	public static void main(String[] args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		String sentence;
+		char letter;
+		int total=0;		
+		
+		System.out.println("Enter a sentence.");
+		sentence = kb.nextLine().toLowerCase();
+		
+		System.out.println("Enter a letter.");
+		letter = (kb.nextLine().toLowerCase()).charAt(0); //i was suprised this worked.
+		
+		for (int x = 0; x<sentence.length(); ++x)
+		{
+			if (sentence.charAt(x)==letter)
+				total++;
+		}
+		System.out.println("There are " + total + " " + letter + "'s in your sentence.");
+	}
+}
